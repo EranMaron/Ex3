0e42a653c1950c56daa4608194e93a44cb197a2e
EranMaron <eran9maron@gmail.com>
2018-05-21 10:22:32 +0300

    Ex3-Layout 3

--- includes/code.js
+++ includes/code.js
@@ -1,4 +1,25 @@
 
-$("document").ready(function() {
-    console.log("Hello world using JQuery!");
-});
+
+window.onload = function() {
+    
+    console.log ("Got In 'onLoad' Function");
+
+    for (i=0; i<4; i++) {
+        var boxObj = document.createElement("article");
+        var contObj = document.getElementById("container");
+        contObj.appendChild(boxObj);
+        boxObj.style.opacity = Math.random();
+    }
+
+
+    var plusBtn = document.getElementById("plusButton");
+    plusBtn.onclick = function () {clickFunc()};
+        
+    function clickFunc() {
+            var box = document.createElement("article");
+            var mainBox = document.getElementById("mainSection");
+            mainBox.appendChild(box);
+            box.style.opacity = Math.random();
+            console.log ("New Box Was Added");
+        }
+}
