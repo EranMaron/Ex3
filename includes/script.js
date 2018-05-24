793a13d2b4f9ed20e7427009aea9041ec1aa6f2f
EranMaron <eran9maron@gmail.com>
2018-05-24 22:59:42 +0300

    Layout3

--- includes/script.js
+++ includes/script.js
@@ -1,12 +1,11 @@
 
-
 window.onload = function() {
     
     console.log ("Got In 'onLoad' Function");
 
     for (i=0; i<4; i++) {
         var boxObj = document.createElement("article");
-        var contObj = document.getElementById("container");
+        var contObj = document.getElementById("main");
         contObj.appendChild(boxObj);
         boxObj.style.opacity = Math.random();
     }
@@ -17,7 +16,7 @@ window.onload = function() {
         
     function clickFunc() {
             var box = document.createElement("article");
-            var mainBox = document.getElementById("mainSection");
+            var mainBox = document.getElementById("main");
             mainBox.appendChild(box);
             box.style.opacity = Math.random();
             console.log ("New Box Was Added");
